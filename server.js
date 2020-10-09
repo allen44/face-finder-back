d783e239f2085dcc2a9cb641a0813f02806c7fd0
Allen <28242507+allen44@users.noreply.github.com>
2020-10-09 08:41:10 -1000

    inital commit

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('this is working');
+})
+
 app.listen(port, () => {
     console.log('app is running on port ', port);
 })
+
+/*  
+/ --> res = this is working
+/signin  --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = user
+
+*/
+ 
