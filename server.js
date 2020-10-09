10dacd3bf967c972d71f1de9c5eb76b14307b385
Allen <28242507+allen44@users.noreply.github.com>
2020-10-09 08:58:55 -1000

    added signin handling

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+const port = 3001;
 
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 app.get('/', (req, res) => {
     res.send('this is working');
 })
 
+///signin  --> POST = success/fail
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success');
+        } else {
+            res.status(400).json('error logging in');
+        }
+})
+
+/
 app.listen(port, () => {
     console.log('app is running on port ', port);
 })
 
 /*  
-/ --> res = this is working
-/signin  --> POST = success/fail
+
 /register --> POST = user
 /profile/:userId --> GET = user
 /image --> PUT = user
