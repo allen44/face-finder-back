66def848a81d35a6e7fc8d78c6946fcaaa1e8f85
Allen <28242507+allen44@users.noreply.github.com>
2020-10-16 09:51:09 -1000

    fix api call

---
+++ controllers/image.js
@@ -0,0 +1,96 @@
+// const {ClarifaiStub} = require("clarifai-nodejs-grpc");
+// const grpc = require("@grpc/grpc-js");
+const Clarifai = require("clarifai");
+const CLARIFAI_API_KEY = '1ecace96280949d684f58ca7f728fe66' //Move this to .env file later
+
+// Construct one of the stubs you want to use
+// const stub = ClarifaiStub.json();
+// const stub = ClarifaiStub.insecureGrpc();
+
+// This will be used by every Clarifai endpoint call.
+// const metadata = new grpc.Metadata();
+// metadata.set("authorization", "Key " + CLARIFAI_API_KEY);
+
+
+const handleApiCall = (req, res ) => {
+    // console.log('req : ', req)
+    // console.log('req.body : ', req.body)
+    // const imageUrl = req.body.id;
+    // const modelIdKeys = {
+    //     generalIdentifier: 'aaa03c23b3724a16a56b629203edc62c',
+    //     faceDectector: 'c0c0ac362b03416da06ab3fa36fb58e3'
+    // }
+    // stub.PostModelOutputs(
+    //     {
+    //         model_id: modelIdKeys.generalIdentifier,
+    //         // version_id: "{THE_MODEL_VERSION_ID}",  // This is optional. Defaults to the latest model version.
+    //         inputs: [
+    //             {data: {image: {url: imageUrl}}}
+    //         ]
+    //     },
+    //     metadata,
+    //     (err, response) => {
+    //         console.log('err : \n', err, '\n\\err')
+    //         console.log('response : \n', response, '\\ response')
+    //         if (err) {
+    //             throw new Error(err);
+    //         }
+    
+    //         if (response.status.code !== 10000) {
+    //             throw new Error("Post model outputs failed, status: " + response.status.description);
+    //         } else { //response.status.code == 10000 
+    //             console.log("Received success status: " + response.status.description + "\n" + response.status.details);
+    //         }
+    
+    //         // Since we have one input, one output will exist here.
+    //         // const output = response.outputs[0];
+    
+    //         console.log("Predicted concepts:");
+    //         for (const concept of output.data.concepts) {
+    //             console.log(concept.name + " " + concept.value);
+    //         }
+    //     }
+    // ).catch( (err) => {
+    //     res.json(err)
+    //     console.log('err in stub.PostModelOutputs :', err);
+    // });
+    handleApiCallDeprecated(req, res);
+}
+
+const handleApiCallDeprecated = (req, res) => {
+    console.log('req.body : ', req.body)
+    const app = new Clarifai.App({
+        apiKey: CLARIFAI_API_KEY
+       });
+    const imageUrl = req.body.input;
+    app.models
+      .predict('c0c0ac362b03416da06ab3fa36fb58e3', imageUrl)
+      .then(data => {
+        console.log(data);
+        res.json(data);
+        })
+      .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    console.log('image.handleImage() :')
+    console.log('req : ', req)
+    console.log('req.body : ', req.body)
+    const { id } = req.body;
+    db ('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        console.log('entries', entries[0]);
+        res.json(entries[0]);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json('unable to get entries');
+    });
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 const { response } = require('express');
+const { handleSignin } = require('./controllers/signin');
+const { handleRegister } = require('./controllers/register');
+const { handleProfileGet } = require('./controllers/profile');
+const { handleApiCall, handleImage } = require('./controllers/image');
 const db = knex({
     client: 'pg',  
     connection: {
@@ -15,114 +19,20 @@ const db = knex({
     }
   });
 
-// console.log(db.select('*').from('users').then(data => {
-//     console.log(data);
-// }).catch( err => console.log(err))) ; 
-
+const PORT = process.env.PORT
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-const port = 3001;
 
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
- 
-///signin  --> POST = success/fail
-app.post('/signin', (req, res) => {
-    console.log(req.body.email);
-    db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        // console.log(data[0]);
-        // console.log(isValid);
-        if (isValid) {
-            return db.select('*').from('users')
-                .where('email', '=', req.body.email)
-                .then(user => {
-                    // console.log(user);
-                    res.json(user[0])
-                })
-                .catch(err => res.status(400).json('unable to get user'))
-        } else {
-            res.status(400).json('wrong credentials')
-        }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
+app.get('/', (req, res) => res.send(database.users))
+app.post('/signin', handleSignin(db, bcrypt));
+app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res ) =>  { handleProfileGet(req, res, db) });
+app.put('/image', (req, res) => { handleImage(req, res, db) });
+app.post('/imageurl', (req, res) => { handleApiCall(req, res) });
 
-//register --> POST = user
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return (
-                trx('users')
-                .returning('*')
-                .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-                }).then(user => {
-                    res.json(user[0]);
-                    })
-            )
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch( err=> res.status(400).json('unable to register'));
-})
-
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params; 
-    db.select('*').from('users').where({id})
-        .then(user => {
-        if(user.length) {
-            res.json(user[0]);
-       } else {
-        res.status(400).json('not found');
-       }
-    }).catch(err => res.status(400).json('error getting user'))
-});;
-
-//image --> PUT = user
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db ('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        console.log(entries[0]);
-        res.json(entries[0]);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json('unable to get entries');
-    });
-});
-
-
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
-
-
-app.listen(port, () => {
-    console.log('app is running on port ', port);
+const port = 3001; 
+app.listen(PORT, () => {
+    console.log('app is running on port ', PORT);
 })
